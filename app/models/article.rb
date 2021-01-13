845f3460c20c4e9374104ff66b9e4b96d747fcc8
Ceejayski <sirchijioke1@gmail.com>
2021-01-13 09:03:42 +0100

    Create Vote model,controller and its associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :categoryings
   has_many :categories, through: :categoryings
+  has_many :votes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
   
   has_many :articles
+  has_many :votes, dependent: :destroy
 end
