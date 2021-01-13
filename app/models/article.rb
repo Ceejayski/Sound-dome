c95653a1c39ceea1a32590f4d75efdcd06d6ec85
Ceejayski <sirchijioke1@gmail.com>
2021-01-13 19:12:33 +0100

    Add validations and solve article index view error

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
   validates :title, length: { maximum: 30 }
-  validates :text, presence: true, length: { minimum: 10 }
+  validates :body, presence: true, length: { minimum: 10 }
   belongs_to :user
   has_many :categoryings
   has_many :categories, through: :categoryings
